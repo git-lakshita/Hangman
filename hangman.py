a4e9ea26c77d65f650f1fe6a70ef8ae08392728f
git-lakshita <57176452+git-lakshita@users.noreply.github.com>
2020-10-01 15:30:47 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,5 +1,10 @@
-def hangman_game():
+from os import system
 
+def cls():
+    system ('cls')
+
+def hangman_game():
+    cls()
     name = input("What is your name?")
     print("Hello, " + name, "It is time to play HamgMan")
     print("Start Guessing...")
@@ -9,30 +14,34 @@ def hangman_game():
     # Creats a variable with an empty value
     guesses = ""
     # Here we set the Secret
-    word = "secret"
+    word = "ramayan"
 
     while turns > 0:
         failed = 0
         for char in word:
             if char in guesses:
-                print(char)
+                print(char, end=" ")
             else:
-                print("_")
+                print("_", end=" ")
                 failed += 1
         if failed == 0:
             print("\nYou WON!     :)")
             break
         guess = input("\nGuess a Character : ")
+        if len(guess) > 1:
+            break
         guesses += guess
-        if (guess not in word):
+        if (guess not in word) and (guess not in guessed_word):
             turns -= 1
             guessed_word.append(guess)
             print("\nWrong Guess :/")
+        cls()
         print("\nYou have ", + turns, "more guesses")
         print("\nWrong Character's Entered : ", guessed_word)
         if turns == 0:
             print("\nGame is OVER, you LOST :o")
     check = input("\nDo you want to play again Y/N?")
-    if check == "Y":
+    if check == "Y" or check == "y" :
         hangman_game()
+
 hangman_game()
